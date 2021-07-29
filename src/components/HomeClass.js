1bcb7bdd7836030aec298ec806b0b64ec8a2ff0d
Matthew McKney <mrmckney@gmail.com>
2021-07-29 17:00:43 -0400

    added class

--- src/components/HomeClass.js
+++ src/components/HomeClass.js
@@ -12,7 +12,7 @@ class HomeClass extends React.Component {
         return (
             <div style={{ padding: '20px' }}>
             <h2>Counter: {counter}</h2>
-            <button onClick={() => this.setState({counter: counter + 1})}>+</button>
+            <button onClick={() => this.setState({counter: counter  + 1})}>+</button>
             &nbsp;
             <button onClick={() => this.setState({ counter: counter - 1})}>-</button>
             </div>
